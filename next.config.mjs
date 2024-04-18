7f9f4960e2e78f5c3192b459a7b07cd4e96a4a8e
Iizalaarab Elhaimeur <ielha003@odu.edu>
2024-04-18 01:51:24 -0400

    Fix Build Steps

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,15 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   output:'export',
-//   basePath: '/vocalshield'
-// };
-// /* module.exports = nextConfig*/
-// export default nextConfig;
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output:'export',
+  basePath: '/vocalshield'
 };
 /* module.exports = nextConfig*/
 export default nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// };
+// /* module.exports = nextConfig*/
+// export default nextConfig;
