10bc8aee35ddfe67a831213be9d5c4ee790c2616
Iizalaarab Elhaimeur <ielha003@odu.edu>
2024-09-13 18:35:55 -0400

    Added Lab Links

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,20 @@
 //Uncommnet me to deploy!!!
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   output:'export',
-//   basePath: '/vocalshield'
-// };
-// /* module.exports = nextConfig*/
-// export default nextConfig;
-
-
-//Uncomment me to develop!!!
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output:'export',
+  basePath: '/vocalshield'
 };
 /* module.exports = nextConfig*/
 export default nextConfig;
+
+
+//Uncomment me to develop!!!
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// };
+// /* module.exports = nextConfig*/
+// export default nextConfig;
 
 // Change these before you push to github because it breaks for some reason. 
