92f2e244415e54526e35e262f52c373a711ce288
Iizalaarab Elhaimeur <ielha003@odu.edu>
2024-09-13 18:35:32 -0400

    Added Lab Links

--- components/labComponent.js
+++ components/labComponent.js
@@ -2,11 +2,10 @@ import { Montserrat } from "next/font/google";
 
 const mont = Montserrat({ subsets: ["latin"] });
 
-
 export default function LabComponent() {
-    return(
-        <div className="lab-container">
-            <h1>LABS GO HERE</h1>
-        </div>
-    )
+  return (
+    <div className="lab-container">
+      <a href="https://docs.google.com/document/d/1lg3FDrrpiiu3TU3NK78PURxRMPS5CAmKJqfNAND5nFI/edit?usp=sharing">LAB OUTLINE LINKS</a>
+    </div>
+  );
 }
