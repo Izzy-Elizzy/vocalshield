931e9659dc2eae27057aafc5801677336b44d4f9
Iizalaarab Elhaimeur <ielha003@odu.edu>
2024-09-13 16:48:22 -0400

    Styling Bio Page Correctly

--- components/Bio.js
+++ components/Bio.js
@@ -1,51 +1,48 @@
-export default function BioComponent() {
+import React from 'react';
+
+const BioComponent = () => {
+  const teamMembers = [
+    {
+      name: 'Joshua Dowell',
+      image: '../media/joshua.gif',
+      bio: 'Hello, my name is Joshua Dowell. I enjoy playing chess, taking walks, reading books, and solving puzzles.',
+    },
+    {
+      name: 'Iizalaarab Elhaimeur',
+      image: '../media/izzy.jpg',
+      bio: "I'm Izzy, a tech enthusiast fascinated by computers and machine intelligence. I enjoy reading literature and manga, with Blame! being one of my favorites. When I'm not messing with code or reading something new, you can find me participating in game jams.",
+    },
+    {
+      name: 'Durel Hairston',
+      image: '../media/durel.jpg',
+      bio: 'I find joy in the excitement of sports and the immersive realms of video games. I also like using my hands, such as improving my PC build and building Legos.',
+    },
+    {
+      name: 'Darell Styles',
+      image: '../media/darrel.jpg',
+      bio: 'Hello! I\'m Darell Styles, a computer science major at ODU. My main hobbies include playing video games, watching movies, and hanging out with friends. My goal is to be the best team member, improve my coding, and learn new skills.',
+    },
+    {
+      name: 'Dima Bochkarev',
+      image: '../media/dima.jpg',
+      bio: "I'm Dima, a senior computer science student at ODU. My hobbies are playing video games, drawing, reading and coding.",
+    },
+  ];
+
   return (
     <div className="bio-container">
-      <div className="bio-item">
-        <div className="bio-pic joshua-pic" />
-        <h2 className="names">Joshua Dowell</h2>
-        <p className="bio-text">
-          Hello, my name is Joshua Dowell. I enjoy playing chess, taking walks,
-          reading books, and solving puzzles.
-        </p>
-      </div>
-      <div className="bio-item">
-        <div className="bio-pic izzy-pic" />
-        <h2 className="names">Iizalaarab Elhaimeur</h2>
-        <p className="bio-text">
-          I'm Izzy, a tech enthusiast fascinated by computers and machine
-          intelligence. I enjoy reading literature and manga, with Blame! being
-          one of my favorites. When I'm not messing with code or reading
-          something new, you can find me participating in game jams.
-        </p>
-      </div>
-      <div className="bio-item">
-        <div className="bio-pic durel-pic" />
-        <h2 className="names">Durel Hairston</h2>
-        <p className="bio-text">
-          I find joy in the excitement of sports and the immersive realms of
-          video games. I also like using my hands, such as improving my PC build
-          and building Legos.
-        </p>
-      </div>
-      <div className="bio-item">
-        <div className="bio-pic darrel-pic" />
-        <h2 className="names">Darell Styles</h2>
-        <p className="bio-text">
-          Hello! I'm Darell Styles, a computer science major at ODU. My main
-          hobbies include playing video games, watching movies, and hanging out
-          with friends. My goal is to be the best team member, improve my
-          coding, and learn new skills.
-        </p>
-      </div>
-      <div className="bio-item">
-        <div className="bio-pic dima-pic" />
-        <h2 className="names">Dima Bochkarev</h2>
-        <p className="bio-text">
-          I'm Dima, a senior computer science student at ODU. My hobbies are
-          playing video games, drawing, reading and coding.
-        </p>
-      </div>
+      {teamMembers.map((member) => (
+        <div className="bio-item" key={member.name}>
+          <div
+            className="bio-pic"
+            style={{ backgroundImage: `url(${member.image})` }}
+          />
+          <h2 className="names">{member.name}</h2>
+          <p className="bio-text">{member.bio}</p>
+        </div>
+      ))}
     </div>
   );
-}
+};
+
+export default BioComponent;
