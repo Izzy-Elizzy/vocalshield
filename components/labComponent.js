687492a7e7cac449a840d547abe0f199355010a2
Iizalaarab Elhaimeur <ielha003@odu.edu>
2024-09-14 02:43:32 -0400

    Added Lab Links

--- components/labComponent.js
+++ components/labComponent.js
@@ -8,6 +8,8 @@ export default function LabComponent() {
       <a href="https://drive.google.com/file/d/1jrDeR96uo0VrBYgFQ2xhr-ljgunQagoT/view?usp=sharing">Durel Lab 1</a>
       <br/>
       <a href="https://drive.google.com/file/d/1qpVE0K76jVsmFGc9p2Ue1sz9tC7aPaan/view?usp=sharing">Joshua Lab 1</a>
+      <br/>
+      <a href="https://docs.google.com/document/d/1Jh58QXdVu-xnc1c59pI10_6_crko4yzawixaWkEpCXQ/edit?usp=sharing">Izzy Lab 1</a>
     </div>
   );
 }
