db59161e0b2d655228a4ec321cc487668eaef722
Iizalaarab Elhaimeur <ielha003@odu.edu>
2024-09-14 02:25:15 -0400

    Added Lab Links

--- components/labComponent.js
+++ components/labComponent.js
@@ -5,7 +5,9 @@ const mont = Montserrat({ subsets: ["latin"] });
 export default function LabComponent() {
   return (
     <div className="lab-container">
-      <a href="https://docs.google.com/document/d/1lg3FDrrpiiu3TU3NK78PURxRMPS5CAmKJqfNAND5nFI/edit?usp=sharing">LAB OUTLINE LINKS</a>
+      <a href="https://drive.google.com/file/d/1jrDeR96uo0VrBYgFQ2xhr-ljgunQagoT/view?usp=sharing">Durel Lab 1</a>
+      <br/>
+      <a href="https://drive.google.com/file/d/1qpVE0K76jVsmFGc9p2Ue1sz9tC7aPaan/view?usp=sharing">Joshua Lab 1</a>
     </div>
   );
 }
