40159bd2bff7bbc8b429d16516cb27129b348d4a
jdowe004 <156550501+jdowe004@users.noreply.github.com>
2024-11-15 19:20:49 -0500

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,26 +1,24 @@
-import { Montserrat } from "next/font/google";
-import { useRouter } from "next/router";
-
-const mont = Montserrat({ subsets: ["latin"] });
-
-export default function Header() {
-
-  const router = useRouter();
-
-    return(
-        <header className="header">
-        <h1 onClick={() => router.push('/')} className="logo">vocalshield</h1>
-        <div className="header-list">
-          <li onClick={() => router.push('/bio')} className="header-item">Bio</li>
-          <li onClick={() => router.push('/feasability')} className="header-item">Feasability</li>
-          <li onClick={() => router.push('/design')} className="header-item">Design</li>
-          <li onClick={() => router.push('/pitch')} className="header-item">Pitch</li>
-          <li onClick={() => router.push('/appendix')} className="header-item">Appendix</li>
-          <li onClick={() => router.push('/links')} className="header-item">Links</li>
-          <li onClick={() => router.push('/outline')} className="header-item">Outline</li>
-          <li onClick={() => router.push('/lab')} className="header-item">Lab</li>
-        </div>
-        <div className="try-button">Try for Free</div>
-      </header>
-    )
-}
+<div class="header">
+  <div class="logo">vocalshield</div>
+  <ul class="header-list">
+    <li class="header-item">Bio</li>
+    <li class="header-item">Feasibility</li>
+    <li class="header-item">Design</li>
+    <li class="header-item">Pitch</li>
+    <li class="header-item">Appendix</li>
+    <li class="header-item">Links</li>
+    <li class="header-item">Outline</li>
+    <li class="header-item">Lab</li>
+    <li>
+      <a
+        href="https://github.com/your-team-repo-url/wiki/User-Manual"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="header-item"
+      >
+        User Manual - Lab 3 & Lab 4
+      </a>
+    </li>
+  </ul>
+  <button class="try-button">Try for Free</button>
+</div>
